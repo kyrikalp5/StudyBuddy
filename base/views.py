a3190307f118c936cd72f6d0aece44a791d6b370
Kyriakos Kalpakoglou <kyrikalp5@gmail.com>
2022-11-28 15:32:27 +0200

    initial commit

--- base/views.py
+++ base/views.py
@@ -6,8 +6,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
-from .models import Room, Topic
-from .forms import RoomForm
+from .models import Room, Topic, Message
+from .forms import RoomForm, MessageForm
 
 # Create your views here.
 
@@ -71,7 +71,19 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
 
 @login_required(login_url='login')
@@ -111,4 +123,31 @@ def deleteRoom(request, pk):
         return redirect('home')
     
     context = {'room': room}
-    return render(request, 'base/delete.html', context)
+    return render(request, 'base/delete_room.html', context)
+
+@login_required(login_url='login')
+def updateMessage(request, pk):
+    message = Message.objects.get(id=pk)
+    room = Room.objects.get(name=message.room)
+    form = MessageForm(instance=message)
+
+    if request.method == 'POST':
+        form = MessageForm(request.POST, instance=message)
+        if form.is_valid():
+            form.save()
+            return redirect('room', pk=room.id)
+
+    context = {'form': form}
+    return render(request, 'base/message_form.html', context)
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+    room = Room.objects.get(name=message.room)
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('room', pk=room.id)
+    
+    context = {'message': message}
+    return render(request, 'base/delete_message.html', context)
